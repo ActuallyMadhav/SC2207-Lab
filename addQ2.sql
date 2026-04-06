bc971abc4d90f31f71fbaccbdd13c408567c99ad
ActuallyMadhav <madhavra.2004@gmail.com>
2026-04-06 10:46:59 +0800

    add query

---
+++ addQ2.sql
@@ -0,0 +1,46 @@
+USE SCSCg1;
+GO
+
+-- Additional Query: For each client, identify products that are currently
+-- available for sale in quantities less than 10% of their average monthly
+-- sales over the last six months.
+
+-- Uses views: vw_OrderClient and vw_OrderProduct
+
+WITH MonthlySales AS (
+    SELECT
+        oc.CID,
+        oc.CompanyName,
+        op.PID,
+        op.ProductName,
+        SUM(op.OrderedQty) * 1.0 / 6 AS AvgMonthlySales
+    FROM vw_OrderClient oc
+    JOIN vw_OrderProduct op ON oc.OID = op.OID
+    WHERE oc.OrderDate >= DATEADD(MONTH, -6, GETDATE())
+      -- TWEAK 1: Don't count cancelled orders in our historical sales average
+      AND oc.OrderStatus != 'Cancelled' 
+    GROUP BY oc.CID, oc.CompanyName, op.PID, op.ProductName
+),
+CurrentStock AS (
+    SELECT
+        CID,
+        PID,
+        SUM(sQty) AS AvailableForSale
+    FROM INVENTORY
+    GROUP BY CID, PID
+)
+SELECT
+    ms.CID,
+    ms.CompanyName,
+    ms.PID,
+    ms.ProductName,
+    -- TWEAK 2: ISNULL replaces a missing inventory record with a 0
+    ISNULL(cs.AvailableForSale, 0) AS AvailableForSale,
+    ROUND(ms.AvgMonthlySales, 2) AS AvgMonthlySales,
+    ROUND(ms.AvgMonthlySales * 0.10, 2) AS Threshold
+FROM MonthlySales ms
+-- TWEAK 2: LEFT JOIN ensures items with sales history but NO current inventory still show up
+LEFT JOIN CurrentStock cs ON cs.CID = ms.CID AND cs.PID = ms.PID
+WHERE ISNULL(cs.AvailableForSale, 0) < ms.AvgMonthlySales * 0.10
+ORDER BY ms.CID, ms.PID;
+GO
