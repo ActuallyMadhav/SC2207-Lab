83612b0043a687ae332d6c226400f018734dd8cb
Samrath-dev <samrath001@e.ntu.edu.sg>
2026-04-06 10:40:21 +0800

    addn query

---
+++ additionalQuery.sql
@@ -0,0 +1,42 @@
+USE SCSCg1;
+GO
+
+-- Additional Query: For each client, identify products that are currently
+-- available for sale in quantities less than 10% of their average monthly
+-- sales over the last six months.
+
+-- Uses views: vw_OrderClient and vw_OrderProduct
+
+WITH MonthlySales AS (
+    SELECT
+        oc.CID,
+        oc.CompanyName,
+        op.PID,
+        op.ProductName,
+        SUM(op.OrderedQty) * 1.0 / 6 AS AvgMonthlySales
+    FROM vw_OrderClient oc
+    JOIN vw_OrderProduct op ON oc.OID = op.OID
+    WHERE oc.OrderDate >= DATEADD(MONTH, -6, GETDATE())
+    GROUP BY oc.CID, oc.CompanyName, op.PID, op.ProductName
+),
+CurrentStock AS (
+    SELECT
+        CID,
+        PID,
+        SUM(sQty) AS AvailableForSale
+    FROM INVENTORY
+    GROUP BY CID, PID
+)
+SELECT
+    ms.CID,
+    ms.CompanyName,
+    ms.PID,
+    ms.ProductName,
+    cs.AvailableForSale,
+    ROUND(ms.AvgMonthlySales, 2) AS AvgMonthlySales,
+    ROUND(ms.AvgMonthlySales * 0.10, 2) AS Threshold
+FROM MonthlySales ms
+JOIN CurrentStock cs ON cs.CID = ms.CID AND cs.PID = ms.PID
+WHERE cs.AvailableForSale < ms.AvgMonthlySales * 0.10
+ORDER BY ms.CID, ms.PID;
+GO
